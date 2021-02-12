df9ed09d9a40180b0d7b33c6cbd4b1ddb75a4ccf
Chad Baker <me@chadbaker.us>
2021-02-12 13:40:21 -0500

    unit testing

--- tests/MongoConnectionTest.php
+++ tests/MongoConnectionTest.php
@@ -1,10 +1,13 @@
 <?php
 
 use MongoDB\Client;
+use PartechGSS\MongoDB\Aggregate\Builder;
+use PHPUnit\Framework\Assert as A;
 use PartechGSS\MongoDB\Connection;
 
 class MongoConnectionTest extends TestCase
 {
+    private $dummy_cursor;
     private $client;
     private $config;
 
@@ -12,15 +15,25 @@ class MongoConnectionTest extends TestCase
     {
         parent::setUp();
         $this->config = config('database.connections.mongodb');
-        $this->client = new Client($this->config['dsn'], (array)@$this->config['options'],(array)@$this->config['driver_options']);
+        $this->client = new Client($this->config['dsn'], (array)@$this->config['options'], (array)@$this->config['driver_options']);
+        $this->dummy_cursor = new class
+        {
+            function setTypeMap($map)
+            {
+            }
+            function toArray()
+            {
+                return [];
+            }
+        };
     }
 
     public function testCanGetCursorForAggregateJson()
     {
         $connection = new Connection($this->client, $this->config['database']);
         $connection->selectCollection('snapshots');
-        $aggregate = ['pipeline' => [['$match' => ['asset_id' => 1]]]];
-        $cursor = $connection->cursor(json_encode($aggregate));
+        $aggregate = ['pipeline' => [['$match' => ['asset_id' => 1]]], 'options' => []];
+        $cursor = $connection->cursor($aggregate);
         $this->assertInstanceOf(\MongoDB\Driver\Cursor::class, $cursor);
     }
 
@@ -28,8 +41,30 @@ class MongoConnectionTest extends TestCase
     {
         $connection = new Connection($this->client, $this->config['database']);
         $connection->selectCollection('device_events');
-        $aggregate = ['pipeline' => [['$match' => ['asset_id' => 1]]]];
-        $array = $connection->select(json_encode($aggregate));
+        $aggregate = ['pipeline' => [['$match' => ['asset_id' => 1]]], 'options' => []];
+        $array = $connection->select($aggregate);
         $this->assertIsArray($array);
+    }
+
+    public function testMongoTypesArePreserved(): void
+    {
+        $collection = $this->createMock(MongoDB\Collection::class);
+        $collection->method('aggregate')->willReturnCallback(function ($params) {
+            A::assertInstanceOf(
+                MongoDB\BSON\UTCDateTime::class,
+                $params[0]['$match']['date']
+            );
+
+            return $this->dummy_cursor;
+        });
+        $client = $this->createMock(MongoDB\Client::class);
+        $database = $this->createMock(MongoDB\Database::class);
+        $database->method('selectCollection')->willReturn($collection);
+        $client->method('selectDatabase')->willReturn($database);
+        $connection = new Connection($client, "ahahahah", "nooooo");
+        $builder = new Builder("breakfasts", $connection);
+        $builder->match([
+            'date' => new MongoDB\BSON\UTCDateTime(strtotime("now") * 1000)
+        ])->get();
     }
 }
